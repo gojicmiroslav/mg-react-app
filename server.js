88943af64ff77b5fd013672a234b86c21838e972
gojicmiroslav <miroslavy2k@gmail.com>
2016-12-03 14:41:24 +0100

    Heroku fix #3

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 var express = require('express');
 
 var app = express();
+var sslRedirect = require('heroku-ssl-redirect');
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next){
-	// console.log(req.headers.host);
-	// console.log(req.hostname);
-	// console.log(req.protocol);
-
-	if(req.protocol === 'http'){
-		next();
-	} else {
-		console.log('http://' + req.hostname + req.url);
-		res.redirect('http://' + req.hostname + req.url);
-	}
-});
+app.use(sslRedirect());
 
 app.use(express.static('public'));
 

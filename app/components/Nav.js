603f3f4ad78719f076133a671ff601346546a21b
gojicmiroslav <miroslavy2k@gmail.com>
2016-12-03 23:45:35 +0100

    Add style to navbar

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, IndexLink } from 'react-router';
 
-const Nav = () => {
-	return (
-		<div>
-			<Link to="/" activeClassName="active">Weather</Link>
-			<Link to="/about" activeClassName="active">About</Link>
-			<Link to="/contact" activeClassName="active">Contact</Link>
-		</div>
-	);
-};
+class Nav extends React.Component {
+	constructor(props){
+		super(props);
+		this.onSearch = this.onSearch.bind(this);
+	}
+
+	onSearch(e){
+		e.preventDefault();
+		var location = this.refs.location.value;
+		console.log("Location: ", location);
+
+		if(location.length > 0) {
+			this.refs.location.value = '';	
+		}
+
+	}
+
+	render(){
+		return (
+			<nav className="navbar navbar-static-top navbar-dark bg-primary">
+	    		<ul className="nav navbar-nav">
+	    			<li className="navbar-brand"><h4 className="font-weight-bold">ReactApp</h4></li>
+	      			<li className="nav-item">
+	        			<IndexLink to="/" activeClassName="active" className="nav-link">Weather API</IndexLink>
+	      			</li>
+	      			<li className="nav-item">
+	        			<Link to="/about" activeClassName="active" className="nav-link">About</Link>
+	      			</li>
+	      			<li className="nav-item">
+	        			<Link to="/contact" activeClassName="active" className="nav-link">Contact</Link>
+	      			</li>
+			    </ul>
+
+	    		<form className="form-inline float-xs-right" onSubmit={this.onSearch}>
+	      			<input className="form-control" type="text" placeholder="Enter city name..." ref="location" />
+	      			<button className="btn btn-outline-secondary" type="submit">Get Weather</button>
+	    		</form>
+	  		</nav>
+		);
+	}
+}
 
 export default Nav;
